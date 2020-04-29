0f6b08a3912d08c88e68376662f49c4f2e7dbe4a
larshp <larshp@hotmail.com>
2020-04-29 11:52:51 +0200

    something like this

--- logic.ts
+++ logic.ts
@@ -11,21 +11,18 @@ function checkEnv(name: string) {
 }
 
 async function run() {
-  console.log("CWD: " + process.cwd());
-
   checkEnv("INPUT_SITE_URL");
   checkEnv("INPUT_CLIENT_ID");
   checkEnv("INPUT_CLIENT_SECRET");
   checkEnv("INPUT_RELATIVE_URL");
   checkEnv("INPUT_GLOB");
 
-  console.dir(process.env.INPUT_SITE_URL);
-  console.dir(process.env.INPUT_CLIENT_ID);
-  console.dir(process.env.INPUT_CLIENT_SECRET);
-  console.dir(process.env.INPUT_RELATIVE_URL);
-  console.dir(process.env.INPUT_GLOB);
+  console.log("CWD: " + process.cwd());
+  console.log("Input SITE_URL: " + process.env.INPUT_SITE_URL);
+  console.log("Input CLIENT_ID: " + process.env.INPUT_CLIENT_ID);
+  console.log("Input RELATIVE_URL: " + process.env.INPUT_RELATIVE_URL);
+  console.log("Input GLOB: " + process.env.INPUT_GLOB);
 
-/*
   sp.setup({
     sp: {
       fetchClientFactory: () => {
@@ -37,17 +34,15 @@ async function run() {
     },
   });
 
-  const folder = await sp.web.getFolderByServerRelativeUrl(process.env.INPUT_RELATIVE_URL);
-*/
+  const folder = sp.web.getFolderByServerRelativeUrl(process.env.INPUT_RELATIVE_URL);
 
   const files = glob.sync(process.env.INPUT_GLOB, {nodir: true})
   console.dir(files);
   for (const filename of files) {
     console.log("Uploading: " + filename);
     const buf = fs.readFileSync(process.cwd() + path.sep + filename);
-//    await folder.files.add(filename, buf, true);
+    await folder.files.add(filename, buf, true);
   }
-
 }
 
 run().then(() => {
