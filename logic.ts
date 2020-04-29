0f5bb34850fb3519401c818e01255d8718dff766
larshp <larshp@hotmail.com>
2020-04-29 10:59:45 +0200

    update

--- logic.ts
+++ logic.ts
@@ -3,32 +3,39 @@ import { sp } from "@pnp/sp";
 import * as path from "path";
 import * as fs from "fs";
 
+function checkEnv(name: string) {
+  if (process.env[name] === undefined) {
+    throw new Error("Set " + name);
+  }
+}
+
 async function run() {
-  console.dir(process.env);
-  console.dir("hello world1");
+  checkEnv("INPUT_SITE_URL");
+  checkEnv("INPUT_CLIENT_ID");
+  checkEnv("INPUT_CLIENT_SECRET");
+  checkEnv("INPUT_RELATIVE_URL");
+  checkEnv("INPUT_GLOB");
+
   console.dir(process.env.INPUT_SITE_URL);
   console.dir(process.env.INPUT_CLIENT_ID);
   console.dir(process.env.INPUT_CLIENT_SECRET);
   console.dir(process.env.INPUT_RELATIVE_URL);
   console.dir(process.env.INPUT_GLOB);
-  console.dir("hello world2");
+
   return;
-
-  if (process.env.SITE_URL === undefined) {
-    throw new Error("Set SITE_URL");
-  }
-
-  const siteUrl = "site_url";
 
   sp.setup({
     sp: {
       fetchClientFactory: () => {
-        return new SPFetchClient(siteUrl, "client_id", "client_secret");
+        return new SPFetchClient(
+          process.env.INPUT_SITE_URL,
+          process.env.INPUT_CLIENT_ID,
+          process.env.INPUT_CLIENT_SECRET);
       },
     },
   });
 
-  const folder = await sp.web.getFolderByServerRelativeUrl("relative_url");
+  const folder = await sp.web.getFolderByServerRelativeUrl(process.env.INPUT_RELATIVE_URL);
 
   const dir = __dirname + path.sep + ".." + path.sep + "glob" + path.sep;
   const files = fs.readdirSync(dir);
